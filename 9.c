0ef4f5eb616b394c5fb106f36b7a1b875cbb3aef
eltonlima01 <eltoncvlima@gmail.com>
2026-02-01 20:29:50 -0300

    Update 9.c

--- 9.c
+++ 9.c
@@ -1,38 +1,68 @@
+/*
+ * Given an integer x, return true if x is a , and false otherwise.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: x = 121
+ * Output: true
+ * Explanation: 121 reads as 121 from left to right and from right to left.
+ * 
+ * Example 2:
+ * 
+ * Input: x = -121
+ * Output: false
+ * Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+ * 
+ * Example 3:
+ * 
+ * Input: x = 10
+ * Output: false
+ * Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+*/
+
+#include <math.h>
+
 char isPalindrome (int x)
 {
     if (x < 0) {
         return 0;
     }
+    else if (x < 10) {
+        return 1;
+    }
 
-////////////////////////////////////////////////////////////////
-
-    char length = 0;
+    // *
 
     int y = x;
+    char length = 0;
 
-    while (y != 0) {
+    do {
         y /= 10;
 
         length++;
     }
+    while (y != 0);
 
-////////////////////////////////////////////////////////////////
+    // *
 
-    char* num = (char*)malloc (length * sizeof (char));
-
-    y = x;
-
-    for (char c = 0; y != 0; c++) {
-        num [c] = y % 10;
-
-        y /= 10;
-    }
-
-    for (char c = 0, d = length - 1; c < length; c++, d--)
+    while (length > 1)
     {
-        if (num [c] != num [d]) {
+        long l = (long)pow (10, length - 1);
+
+        if (l == 0) {
+            return 1;
+        }
+
+        if ((x / l) != (x % 10)) {
             return 0;
         }
+
+        x %= l;
+        x /= 10;
+
+        length -= 2;
     }
 
     return 1;
